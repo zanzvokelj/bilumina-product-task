215156b038025fbadf06162e8aa269cb477a55ad
ZanZvokelj <zan.zvokelj@academiavss.si>
2026-02-24 16:45:17 +0100

    update

--- src/api/fetchItems.ts
+++ src/api/fetchItems.ts
@@ -1,7 +1,8 @@
 import type { ApiResponse } from "../types/api";
 
-const API_URL =
-  "https://egi.bilumina.com/mw/api/v1/items/get?key=bf84d5ef-7fe2-4609-8b75-49279dd3271e";
+const API_KEY = import.meta.env.BILUMINA_API_KEY;
+
+const API_URL = `https://egi.bilumina.com/mw/api/v1/items/get?key=${API_KEY}`;
 
 export async function fetchItems(): Promise<ApiResponse> {
   const response = await fetch(API_URL);
